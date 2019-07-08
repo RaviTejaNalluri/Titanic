b9b2ad934f61d32a079025776e9393610ad20d1e
Ravi Teja <ravi.nalluri1998@gmail.com>
2019-07-08 20:51:32 +0530

    added data

--- titanic.py
+++ titanic.py
@@ -4,5 +4,9 @@ import pandas as pd
 data=pd.read_csv('/home/ravi/Documents/Titanic/gender_submission.csv',dtype=str)
 print data
 
-data.dropna(axis='rows')
+datan=data.dropna(axis='rows')
 print data
+datat=pd.read_csv('/home/ravi/Documents/Titanic/train.csv',dtype=str)
+print datat
+datatp=datat.dropna(axis='columns')
+print datatp
