8a48d3d0e97b552c77b1e37c0f3c067c20f5dfd1
Ravi Teja <ravi.nalluri1998@gmail.com>
2019-07-08 19:54:19 +0530

    added data

--- titanic.py
+++ titanic.py
@@ -1,4 +1,5 @@
 import numpy as np
 import pandas as pd
 
-data=np.genfromtxt('/home/ravi/Documents/Titanic/train.csv',delimiter=",")
+data=np.genfromtxt('/home/ravi/Documents/Titanic/gender_submission.csv',delimiter=",")
+print data
