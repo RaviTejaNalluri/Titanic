18d51d200cb5b1af2e8f54521c9718804d347c11
Ravi Teja <ravi.nalluri1998@gmail.com>
2019-07-16 18:34:40 +0530

    Updated

--- titanic.py
+++ titanic.py
@@ -8,9 +8,8 @@ from sklearn.model_selection import train_test_split
 data=pd.read_csv('/home/ravi/Documents/Titanic/train.csv')
 #print data.head()
 
-data.drop('Name',axis=1,inplace=True)
-data.drop('Ticket',axis=1,inplace=True)
-data.drop('Cabin',axis=1,inplace=True)
+
+
 #print data.head()
 
 #calculating age where data is misssing.Taking average age wrt pclass
@@ -25,9 +24,10 @@ def calc_age(columns):
         else:
             return 24
     else:
-        return age
-    data['Age']=data[['Age','Pclass']].apply(calc_age,axis=1)
+        return Age
+
 #label encoding to give nummerical values to gender snfd embarked
+'''
 le=preprocessing.LabelEncoder()
 le.fit(data['Sex'])
 data['Sex']=le.transform(data['Sex'])
@@ -46,3 +46,40 @@ model = LogisticRegression()
 model.fit(train_X,train_Y)
 prediction=model.predict(test_X)
 print 'The accuracy of the Logistic Regression is',metrics.accuracy_score(prediction,test_Y)
+'''
+
+data=pd.read_csv('/home/ravi/Documents/Titanic/train.csv')
+
+
+data.drop('Name',axis=1,inplace=True)
+data.drop('Ticket',axis=1,inplace=True)
+data['Age'] = data[['Age','Pclass']].apply(calc_age,axis=1)
+data.drop('Cabin',axis=1,inplace=True)
+data = data.dropna()
+
+
+
+cumsum = data.groupby('Sex')['Survived'].cumsum() - data['Survived']
+cumcnt = data.groupby('Sex').cumcount()
+data['Sex'] = cumsum/cumcnt
+data['Sex'].fillna(0.3343, inplace = True)
+
+cumsum = data.groupby('Embarked')['Survived'].cumsum() - data['Survived']
+cumcnt = data.groupby('Embarked').cumcount()
+data['Embarked'] = cumsum/cumcnt
+data['Embarked'].fillna(0.3343, inplace = True)
+
+
+print data.head()
+
+from sklearn.linear_model import LogisticRegressionCV
+LogisticRegressionCV(Cs=10)
+from sklearn.model_selection import train_test_split
+train_X,test_X, train_Y, test_Y = train_test_split(data.drop('Survived',axis=1),
+                                                    data['Survived'], test_size=0.30,
+                                                    random_state=101)
+from sklearn import metrics
+model = LogisticRegressionCV()
+model.fit(train_X,train_Y)
+prediction=model.predict(test_X)
+print('The accuracy of the Logistic Regression is',metrics.accuracy_score(prediction,test_Y))
